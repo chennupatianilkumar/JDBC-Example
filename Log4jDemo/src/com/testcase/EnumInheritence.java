cb6d3fd4862ce6bfbf82fcb26eead8e9c8918e15
Anil kumar <chennupatianilkumar514@gmail.com>
2018-04-24 00:08:39 -0500

    First commit

--- Log4jDemo/src/com/testcase/EnumInheritence.java
+++ Log4jDemo/src/com/testcase/EnumInheritence.java
@@ -4,6 +4,7 @@ public class EnumInheritence {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//dcdsvdv
 		System.out.println(BaseEnum.ONE);
 		System.out.println(DerivedEnum.ONE);
 		System.out.println(DerivedEnum.TWO);
